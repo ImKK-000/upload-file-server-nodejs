bd3eaed1b90722f1ab7d07f557f45f699e102a6c
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-18 08:21:43 +0700

    Update project

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const fs = require('fs')
 const md5File = require('md5-file')
 const fileUpload = require('express-fileupload')
+const sleep = require('sleep')
 const app = express()
 
 const filesPath = path.join(__dirname, 'upload_files')
@@ -20,67 +21,77 @@ app
 app
   .post('/api/process', async (req, res) => {
     if (Object.keys(req.body).length) {
-      const { id, type, settings, files: { fileName, fileExt } } = req.body
+      const { id, type, settings, files: { fileId, fileExt } } = req.body
+      const fileName = `${fileId}.${fileExt}`
       const file = path.join(filesPath, fileName)
       const outFile = path.join(filesPath, `temp_process.${fileExt}`)
 
-      // NOTE: unpack settings
-      Object.keys(settings).map((key) => {
-        settings[key] = settings[key].value
-        return true
-      })
-
       if (fileName.length == 0) {
-        res.json({ status: 0 })
+        res.json({ status: 1 })
+        return
+      }
+
+      if (!fs.existsSync(file)) {
+        res.json({ status: 1 })
         return
       }
 
       // NOTE: function stream process
       await cv.readImage(file, (err, im) => {
-        if (err) {
-          res.json({ status: 0 })
-          return
+        try {
+          sleep.msleep(500)
+
+          if (err) {
+            res.json({ status: 1 })
+            return
+          }
+          if (type === 'ResizeFunction') {
+            const { widthPercent, heightPercent } = settings
+            const [height, width] = im.size()
+            const computedWidthSize = widthPercent > 0 ? width * widthPercent / 100 : width
+            const computedHeightSize = heightPercent > 0 ? height * heightPercent / 100 : height
+            im.resize(width + computedWidthSize, height + computedHeightSize)
+          } else if (type === 'GaussianBlurFunction') {
+            const { sigma } = settings
+            im.gaussianBlur([sigma, sigma])
+          } else if (type === 'RotateFunction') {
+            const { angle } = settings
+            im.rotate(angle)
+          } else if (type === 'ConvertGrayScaleFunction') {
+            im.convertGrayscale()
+          } else if (type === 'ConvertHSVscale') {
+            im.convertHSVscale()
+          }
+
+          im.save(outFile)
+
+          const hash = md5File.sync(outFile)
+          const newFileName = `${hash}.${fileExt}`
+          const newFile = path.join(filesPath, newFileName)
+          fs.renameSync(outFile, newFile)
+
+          res.json({
+            fileId: hash,
+            fileExt
+          })
         }
-        if (type === 'ResizeFunction') {
-          const { widthPercent, heightPercent } = settings
-          const [height, width] = im.size()
-          const computedWidthSize = widthPercent > 0 ? width * widthPercent / 100 : width
-          const computedHeightSize = heightPercent > 0 ? height * heightPercent / 100 : height
-          im.resize(width + computedWidthSize, height + computedHeightSize)
-        } else if (type === 'GaussianBlurFunction') {
-          const { sigmaX, sigmaY } = settings
-          im.gaussianBlur([sigmaX, sigmaY])
-        } else if (type === 'RotateFunction') {
-          const { angle } = settings
-          im.rotate(angle)
-        } else if (type === 'ConvertGrayScaleFunction') {
-          im.convertGrayscale()
-        } else if (type === 'ConvertHSVscale') {
-          im.convertHSVscale()
+        catch (error) {
+          res.json({
+            status: 1
+          })
         }
-
-        im.save(outFile)
-
-        const hash = md5File.sync(outFile)
-        const newFileName = `${hash}.${fileExt}`
-        const newFile = path.join(filesPath, newFileName)
-        fs.renameSync(outFile, newFile)
-
-        res.json({
-          fileId: hash,
-          fileName: newFileName,
-          fileExt
-        })
       })
 
       return
     }
 
-    res.json('yo')
+    res.json({ status: 1 })
   })
 
   .post('/upload', (req, res) => {
-    if (!req.files) return res.status(400).send('No files were uploaded.')
+    if (!req.files) return res
+      .status(400)
+      .json({ status: 1 })
 
     const { name, mimetype, md5, mv } = req.files.file;
     const fileExt = mimetype.match(/(png|jpg|jpeg)/g)[0]
@@ -91,15 +102,20 @@ app
       console.log('uploaded new file!');
 
       mv(absolutePath, function (err) {
-        if (err) return res.status(500).send(err)
+        if (err) return res
+          .status(500)
+          .json({
+            status: 1
+          })
       })
     }
 
-    return res.status(200).json({
-      fileId: md5,
-      fileName,
-      fileExt
-    })
+    return res
+      .status(200)
+      .json({
+        fileId: md5,
+        fileExt
+      })
   })
 
 app.listen(9999)
